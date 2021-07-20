e533de10573bb39e63df5ffb7ca0275f2dee1bfc
Anibal Robleto <anibaldelhi@outlook.com>
2021-07-20 11:28:47 -0600

    Agregar página de Error 404.

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,30 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+
 export default function ErrorPage(){
     return (
-        <div>
-            <h1>Error 404</h1>
-        </div>
+        <Card>
+            <CardActionArea>
+                <CardContent>
+                    <Typography gutterBottom variant="h4" component="h2">
+                        Página no encontrada :(
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Lo sentimos, la página que intentas solicitar no fue encontrada.
+                    </Typography>
+                </CardContent>
+                </CardActionArea>
+                <CardActions>
+                <Button component={Link} to="/" size="small" color="primary">
+                    Ir a inicio
+                </Button>
+            </CardActions>
+        </Card>
     )
 }
