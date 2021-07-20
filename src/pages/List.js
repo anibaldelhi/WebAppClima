303f536fac0cc657db8f9ea800b9cbe6a5e8710f
Anibal Robleto <anibaldelhi@outlook.com>
2021-07-20 08:22:51 -0600

    Agregar título a página List.

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,8 +1,10 @@
 import DataTable from "../components/DataTable";
+import Typography from '@material-ui/core/Typography';
 
 export default function ListPage(){
     return (
         <div>
+            <Typography variant="h4" gutterBottom>Datos hidrometeorológicos</Typography>
             <DataTable />
             
         </div>
